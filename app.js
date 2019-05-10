b1a33482664b5ce29cabcffa83a5c0733ce1a8d0
sophyme <46559970+sophyme@users.noreply.github.com>
2019-05-10 14:37:56 +0900

    js - add a to do
    I did the same thing, but it didn't execute.
    I don't know what's wrong....

--- app.js
+++ app.js
@@ -1,123 +1,32 @@
-// Select the Elements
+// Select elements
+/*
 const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
-const input = document.getElementById("input");
+const input = document.getElementById("input"); */
 
-// Classes names
-const CHECK = "fa-check-circle";
-const UNCHECK = "fa-circle";
-const LINE_THROUGH = "lineThrough";
+// Add a to-do
+const list = document.getElementById("list");
 
-// Variabled
-let LIST, id;
+function addToDo(toDo) {
+  const text = `
+     <li class="item">
+            <i class="co far fa-circle" job="complete" />
+            <p class="text"> ${toDo} </p>
+            <i class="de far fa-trash-alt" job="delete" />
+     </li>`;
 
-// get item from localstorage
-let data = localStorage.getItem("TODO");
-
-// check if data is not empty
-if (data) {
-  LIST = JSON.parse(date);
-  id = LIST.length; // set the id to the last one in the list
-  loadList(LIST); // load the list to the user interface
-} else {
-  // if data isn't empty
-  LIST = [];
-  id = 0;
-}
-
-// load items to the user's interface
-function loadList(array) {
-  array.forEach(function(item) {
-    addToDo(item.name, item.id, item.done, item.trash);
-  });
-}
-
-// clear the local storage
-clear.addEventListener("click", function() {
-  localStorage.clear();
-  location.reload();
-});
-
-// Show todays date
-const options = { weekday: "long", month: "short", day: "numeric" };
-const today = new Date();
-
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
-
-// Add to do function
-
-function addToDo(toDo, id, done, trash) {
-  if (trash) {
-    return;
-  }
-
-  const DONE = done ? CHECK : UNCHECK;
-  const LINE = done ? LINE_THROUGH : "";
-
-  const item = `<li class="item">
-                <i class="far ${DONE} co" job="complete" id="${id}"></i>
-                <p class="text ${LINE}">${toDo}</p>
-                <i class="far fa-trash-alt" job="delete" id="${id}"></i>
-            </li>`;
   const position = "beforeend";
 
-  list.insertAdjacentHTML(position, item);
+  list.insertAdjacentHTML(position, text);
 }
 
-// add an item to the list user the enter key
 document.addEventListener("keyup", function(event) {
   if (event.keyCode == 13) {
-    const toDo = input.nodeValue;
-
-    // if the input isn't empty
+    const toDo = input.value;
     if (toDo) {
-      addToDo(toDo, id, false, false);
-
-      LIST.push({
-        name: toDo,
-        id: id,
-        done: false,
-        trash: false
-      });
-
-      //add item to localstorage (this code must be added where the LIST array is updated)
-      localStorage.setItem("TODO", JSON.stringify(LIST));
-
-      id++;
+      addToDo(toDo);
     }
     input.value = "";
   }
-});
-
-// complete to do
-function completeToDo(element) {
-  element.classList.toggle(CHECK);
-  element.classList.toggle(UNCHECK);
-  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGHT);
-
-  LIST[element.id].done = LIST[element.id].done ? false : true;
-}
-
-//remove to do
-function removeToDo(element) {
-  element.parentNode.parentNode.removeChild(element.parentNode);
-
-  LIST[element.id].trash = true;
-}
-
-// target the items created dynamically
-
-list.addEventListener("click", function(event) {
-  const element = event.target; // return the clicked element inside list
-  const elementJob = element.attributes.job.value; // complete or delete
-
-  if (elementJob == "complete") {
-    comopleteToDo(element);
-  } else if (elementJob == "delete") {
-    removeToDo(elemnt);
-  }
-
-  // add item to localstorage (this code must be added where the LIST array is updated)
-  localStorage.setItem("TODO", JSON.stringify(LIST));
 });
