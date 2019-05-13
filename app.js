7743a4388385e030b4142e16b0f2235ee6f478b6
sophyme <46559970+sophyme@users.noreply.github.com>
2019-05-13 14:06:16 +0900

    addtodo is done
    but  js don't execute.

--- app.js
+++ app.js
@@ -7,13 +7,23 @@ const input = document.getElementById("input"); */
 
 // Add a to-do
 const list = document.getElementById("list");
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle";
+const LINE_THROUGH = "lineThrough";
 
-function addToDo(toDo) {
+function addToDo(toDo, id, done, trash) {
+  if (trash) {
+    return;
+  }
+
+  const DONE = done ? CHECK : UNCHECK;
+  const LINE = dond ? LINE_THROUGH : "";
+
   const text = `
      <li class="item">
-            <i class="co far fa-circle" job="complete" />
-            <p class="text"> ${toDo} </p>
-            <i class="de far fa-trash-alt" job="delete" />
+            <i class="far ${DONE} co" job="complete" id="${id}"></i>
+            <p class="text ${LINE}"> ${toDo} </p>
+            <i class="far fa-trash-alt de" job="delete" id="${id}"></i>
      </li>`;
 
   const position = "beforeend";
@@ -25,8 +35,29 @@ document.addEventListener("keyup", function(event) {
   if (event.keyCode == 13) {
     const toDo = input.value;
     if (toDo) {
-      addToDo(toDo);
+      addToDo(toDo, id, false, false);
+      LIST.push({
+        name: toDo,
+        id: id,
+        done: false,
+        trash: false
+      });
     }
     input.value = "";
+    id++;
   }
 });
+
+element.classList.toggle(CLASS);
+
+function completeToDo(element) {
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+  LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+  LIST[element.id].trash = true;
+}
